090583cdac07c0694380439fb64029c44d4b8098
weswigham <wwigham@gmail.com>
2014-01-21 17:52:26 -0500

    Video player now expects what the streaming app sends

--- video_player.rb
+++ video_player.rb
@@ -1,4 +1,5 @@
 require 'mycroft'
+require 'vlc-client'
 
 class VideoPlayer < Mycroft::Client
 
@@ -9,19 +10,40 @@ class VideoPlayer < Mycroft::Client
     @cert = ''
     @manifest = './app.json'
     @verified = false
+    @vlc = VLC::System.new(fullscreen: true)
+    @status = 'stopped'
     super
   end
 
   def connect
     # Your code here
   end
+  
+  def on_event_loop
+    if @status=='playing'
+      if (!vlc.playing?)
+        @status = 'stopped'
+        up
+      end
+    end
+  end
 
   def on_data(parsed)
     if parsed[:type] == 'APP_DEPENDENCY'
       up
-    elsif parsed[:type] == "MSG_QUERY" and parsed[:type]['action'] == 'start_stream'
-      data = parsed[:data]['data']
-      `vlc rtsp://#{data['host']}:#{data['port']}`
+    elsif parsed[:type] == "MSG_QUERY" and parsed[:type]['action'] == 'stream'
+      if (!vlc.playing?)
+        @status = 'playing'
+        in_use
+        data = parsed[:data]['data']
+        @vlc.play(data)
+      end
+    elsif parsed[:type] == "MSG_QUERY" and parsed[:type]['action'] == 'halt'
+      if (vlc.playing?)
+        @vlc.stop
+        @status = 'stopped'
+        up
+      end
     end
   end
 
