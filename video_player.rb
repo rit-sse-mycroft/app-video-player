37884db3dba0b505a94ba3797c22b3ea42ea930e
weswigham <wwigham@gmail.com>
2014-01-23 16:57:11 -0500

    Can play videos from the streamer

--- video_player.rb
+++ video_player.rb
@@ -12,34 +12,46 @@ class VideoPlayer < Mycroft::Client
     @verified = false
     @vlc = VLC::System.new(fullscreen: true)
     @status = 'stopped'
+    @threaded = true
     super
   end
 
   def connect
-    # Your code here
+      Thread.new do
+        loop do
+          if @status=='playing'
+            if (!@vlc.playing?)
+              @status = 'stopped'
+              up
+            end
+          end
+        end
+      end
   end
   
   def on_event_loop
-    if @status=='playing'
-      if (!vlc.playing?)
-        @status = 'stopped'
-        up
-      end
-    end
+
   end
 
   def on_data(parsed)
     if parsed[:type] == 'APP_DEPENDENCY'
+     # meh
+    elsif parsed[:type] == 'APP_MANIFEST_OK'
+      puts "Going up!"
       up
-    elsif parsed[:type] == "MSG_QUERY" and parsed[:type]['action'] == 'stream'
-      if (!vlc.playing?)
-        @status = 'playing'
-        in_use
-        data = parsed[:data]['data']
+    elsif parsed[:type] == "MSG_QUERY" and parsed[:data]['action'] == 'video_stream'
+      if (!@vlc.playing?)
+        puts "Starting the video!"
+        Thread.new do
+          sleep 5 # Give the video a few seconds to start
+          @status = 'playing'
+        end
+        in_use(30)
+        data = parsed[:data]['data']['url']
         @vlc.play(data)
       end
-    elsif parsed[:type] == "MSG_QUERY" and parsed[:type]['action'] == 'halt'
-      if (vlc.playing?)
+    elsif parsed[:type] == "MSG_QUERY" and parsed[:data]['action'] == 'halt'
+      if (@vlc.playing?)
         @vlc.stop
         @status = 'stopped'
         up
@@ -48,8 +60,12 @@ class VideoPlayer < Mycroft::Client
   end
 
   def on_end
-    # Your code here
+
   end
 end
 
 Mycroft.start(VideoPlayer)
+
+while true
+
+end
